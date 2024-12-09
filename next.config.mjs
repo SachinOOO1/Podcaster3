7f8d0b438dad9605c25877df13f744240ada3c8d
Shubham Aggarwal <aggarwalshubham026@gmail.com>
2024-12-09 13:46:33 +0530

    Changes in Next Config

--- next.config.mjs
+++ next.config.mjs
@@ -7,11 +7,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'lovely-flamingo-139.convex.cloud'
-      },
-      {
-        protocol: 'https',
-        hostname: 'sleek-capybara-771.convex.cloud'
+        hostname: 'rightful-gopher-525.convex.cloud'
       },
       {
         protocol: 'https',
